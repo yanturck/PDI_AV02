9c1498d35add1be9694d72cb2093106fa6bad40e
Yan Rocha <yanpimenta7@gmail.com>
2021-12-08 17:10:39 -0300

    Implementação Dithering

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
 import util.Imagem;
 import util.Moforlogia;
+import util.Dithering;
 
 public class Main {
     static String IMAGEM_LENNA = "img/lenna.png";
 
     public static void main(String args[]) {
-        testeMoforlogia();
+        // testeMoforlogia();
+        testeDithering();
     }
 
     private static void testeMoforlogia() {
@@ -19,5 +21,21 @@ public class Main {
         mb.fechamento(img, ee).mostrar("Fechamento");
         mb.gradiente(img, ee).mostrar("Gradiente");
         mb.smoothing(img, ee).mostrar("Smoothing");
+    }
+
+    private static void testeDithering() {
+        Imagem img = new Imagem(Main.IMAGEM_LENNA);
+        img.mostrar("Imagem Original");
+        Dithering d = new Dithering();
+        int[][] D2 = {{0, 2},{3, 1}};
+        int[][] D3 = {{6, 8, 4},{1, 0, 3},{5, 2, 7}};
+        int[][] D4 = {{0, 8, 2, 10},{12, 4, 14, 6},{3, 11, 1, 9},{15, 7, 13, 5}};
+        d.aperiodico(img).mostrar("Aperiodica");
+        d.periodicoDisperso(img, D2).mostrar("Periodico Disperso - D2");
+        // d.periodicoDisperso(img, D3).mostrar("Periodico Disperso - D3");
+        // d.periodicoDisperso(img, D4).mostrar("Periodico Disperso - D4");
+        d.periodicoAglomerado(img, D2).mostrar("Periodico Aglomerado - D2");
+        // d.periodicoAglomerado(img, D3).mostrar("Periodico Aglomerado - D3");
+        // d.periodicoAglomerado(img, D4).mostrar("Periodico Aglomerado - D4");
     }
 }
--- util/Dithering.java
+++ util/Dithering.java
@@ -5,5 +5,114 @@ package util;
 // aperiódico
 
 public class Dithering {
-    
+    private static int BLACK = 0;
+    private static int WHITE = 255;
+
+    public int[][][] quantizar(Imagem in, int L, int C) {
+        int alt = in.getAltura();
+        int larg = in.getLargura();
+        int nCanais = in.getNCanais();
+        int matIn[][][] = in.getMatriz();
+        int matOut[][][] = new int[nCanais][alt][larg];
+        int faixa = (L*C)-1;
+
+        for (int c = 0; c < nCanais; c++) {
+            for (int y = 0; y < alt; y++) {
+                for (int x = 0; x < larg; x++) {
+                    matOut[c][y][x] = (int)((matIn[c][y][x]*faixa)/255);
+                }
+            }
+        }
+        return matOut;
+    }
+
+    public Imagem periodicoDisperso(Imagem in, int D[][]) {
+        int alt = in.getAltura();
+        int larg = in.getLargura();
+        int nCanais = in.getNCanais();
+        int N = D.length;
+        int matIn[][][] = quantizar(in, N, N);
+        int i, j;
+
+        for (int c = 0; c < nCanais; c++) {
+            for (int y = 0; y < alt; y++) {
+                for (int x = 0; x < larg; x++) {
+                    i = x % N;
+                    j = y % N;
+                    if (matIn[c][y][x] > D[j][i]) {
+                        matIn[c][y][x] = WHITE;
+                    } else {
+                        matIn[c][y][x] = BLACK;
+                    }
+                }
+            }
+        }
+
+        return new Imagem(matIn);
+    }
+
+    public Imagem periodicoAglomerado(Imagem in, int[][] D) {
+        int alt = in.getAltura();
+        int larg = in.getLargura();
+        int nCanais = in.getNCanais();
+        int N = D.length;
+        int matIn[][][] = quantizar(in, N, N+1);
+        int matOut[][][] = new int[nCanais][alt*N][larg*N];
+        int yOut, xOut;
+
+        for (int c = 0; c < nCanais; c++) {
+            yOut = 0;
+            for (int y = 0; y < alt; y++) {
+                xOut = 0;
+                for (int x = 0; x < larg; x++) {
+                    for (int j = 0; j < N; j++) {
+                        for (int i = 0; i < N; i++) {
+                            if (matIn[c][y][x] < D[j][i]) matOut[c][yOut+j][xOut+i] = WHITE;
+                            else matOut[c][yOut+j][xOut+i] = BLACK;
+                        }
+                    }
+                    xOut += N;
+                }
+                yOut += N;
+            }
+        }
+        return new Imagem(matOut);
+    }
+
+    public Imagem aperiodico(Imagem in) {
+        int alt = in.getAltura();
+        int larg = in.getLargura();
+        int nCanais = in.getNCanais();
+        int matIn[][][] = in.getMatriz();
+        int error, threshold;
+        int count = 0;
+
+        for (int c = 0; c < nCanais; c++) {
+            for (int y = 0; y < alt-1; y++) {
+                for (int x = 0; x < larg-1; x++) {
+                    count += matIn[c][y][x];
+                }
+            }
+        }
+
+        threshold = count/(nCanais*alt*larg);
+
+        for (int c = 0; c < nCanais; c++) {
+            for (int y = 0; y < alt-1; y += 2) {
+                for (int x = 0; x < larg-1; x += 2) {
+                    if (matIn[c][x][y] < threshold) {
+                        error = matIn[c][x][y] - BLACK;
+                        matIn[c][x][y] = BLACK;
+                    } else {
+                        error = matIn[c][x][y] - WHITE;
+                        matIn[c][x][y] = WHITE;
+                    }
+                    matIn[c][x+1][y] += (3/8)*error;
+                    matIn[c][x][y+1] += (3/8)*error;
+                    matIn[c][x+1][y+1] += (2/8)*error;
+                }
+            }
+        }
+        return new Imagem(matIn);
+    }
 }
